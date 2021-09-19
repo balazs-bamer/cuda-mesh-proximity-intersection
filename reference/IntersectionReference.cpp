8073aa2a83631a510b6d3f3080e58ec98a71ae44
Balázs Bámer <balazs@bamer.hu>
2021-09-19 17:13:13 +0200

    Added triangle division, something is wrong.

--- reference/IntersectionReference.cpp
+++ reference/IntersectionReference.cpp
@@ -2,6 +2,7 @@
 
 #include "../../../repos/stl_reader/stl_reader.h"
 #include <map>
+#include <list>
 #include <array>
 #include <deque>
 #include <chrono>
@@ -13,7 +14,6 @@
 #include <Eigen/Dense>
 
 using Vertex = Eigen::Vector3f;
-using Quaternion = Eigen::Quaternion<float>;
 using Transform = Eigen::Transform<float, 3, Eigen::Affine>;
 using Triangle = std::array<Vertex, 3u>;
 using CudaTriangle = Vertex*;
@@ -25,156 +25,14 @@ using Mesh = std::vector<Triangle>;
 constexpr float    cgPi                            =    3.1415926539f;
 constexpr int32_t  cgSphereSectors                 =    11;
 constexpr int32_t  cgSphereBelts                   =    5;
+constexpr float    cgMaxSphereRadius               =   17.0f;
+constexpr float    cgMaxTriangleSide               = cgMaxSphereRadius * 2.0f;
 constexpr float    cgCalculateSpheresInflate       =    0.51f;
 constexpr int32_t  cgCalculateSpheresNeighbours    =    3;
 constexpr float    cgApproximateLeaveInPlaceFactor =    0.01f;
-constexpr uint32_t cgApproximateResultSize         =   32u;
+constexpr uint32_t cgApproximateResultSize         =   32u;       // TODO consider remove
 constexpr int32_t  cgApproximateIterations         = 2222u;
 constexpr float    cgApproximateTemperatureFactor  =    0.01f;
-constexpr float    cgEpsilonDistanceFromSideFactor =    0.001f;
-constexpr float    cgEpsilonPlaneIntersectionSine  =    0.001f;
-constexpr uint32_t cgSignumZero     = 0u;
-constexpr uint32_t cgSignumPlus     = 1u;
-constexpr uint32_t cgSignumMinus    = 2u;
-constexpr uint32_t cgSignumShift0   = 0u;
-constexpr uint32_t cgSignumShift1   = 2u;
-constexpr uint32_t cgSignumShift2   = 4u;
-constexpr uint32_t cgSignumAllZero  = (cgSignumZero  << cgSignumShift0) | (cgSignumZero  << cgSignumShift1) | (cgSignumZero  << cgSignumShift2);
-constexpr uint32_t cgSignumAllPlus  = (cgSignumPlus  << cgSignumShift0) | (cgSignumPlus  << cgSignumShift1) | (cgSignumPlus  << cgSignumShift2);
-constexpr uint32_t cgSignumAllMinus = (cgSignumMinus << cgSignumShift0) | (cgSignumMinus << cgSignumShift1) | (cgSignumMinus << cgSignumShift2);
-
-constexpr uint32_t cgSignumSelect0a = (cgSignumPlus  << cgSignumShift0) | (cgSignumMinus << cgSignumShift1) | (cgSignumMinus << cgSignumShift2);
-constexpr uint32_t cgSignumSelect0b = (cgSignumMinus << cgSignumShift0) | (cgSignumPlus  << cgSignumShift1) | (cgSignumPlus  << cgSignumShift2);
-constexpr uint32_t cgSignumSelect0c = (cgSignumZero  << cgSignumShift0) | (cgSignumPlus  << cgSignumShift1) | (cgSignumPlus  << cgSignumShift2);
-constexpr uint32_t cgSignumSelect0d = (cgSignumZero  << cgSignumShift0) | (cgSignumMinus << cgSignumShift1) | (cgSignumMinus << cgSignumShift2);
-constexpr uint32_t cgSignumSelect0e = (cgSignumPlus  << cgSignumShift0) | (cgSignumZero  << cgSignumShift1) | (cgSignumZero  << cgSignumShift2);
-constexpr uint32_t cgSignumSelect0f = (cgSignumMinus << cgSignumShift0) | (cgSignumZero  << cgSignumShift1) | (cgSignumZero  << cgSignumShift2);
-constexpr uint32_t cgSignumSelect0g = (cgSignumZero  << cgSignumShift0) | (cgSignumPlus  << cgSignumShift1) | (cgSignumMinus << cgSignumShift2);
-constexpr uint32_t cgSignumSelect0h = (cgSignumZero  << cgSignumShift0) | (cgSignumMinus << cgSignumShift1) | (cgSignumPlus  << cgSignumShift2);
-
-constexpr uint32_t cgSignumSelect1a = (cgSignumPlus  << cgSignumShift1) | (cgSignumMinus << cgSignumShift0) | (cgSignumMinus << cgSignumShift2);
-constexpr uint32_t cgSignumSelect1b = (cgSignumMinus << cgSignumShift1) | (cgSignumPlus  << cgSignumShift0) | (cgSignumPlus  << cgSignumShift2);
-constexpr uint32_t cgSignumSelect1c = (cgSignumZero  << cgSignumShift1) | (cgSignumPlus  << cgSignumShift0) | (cgSignumPlus  << cgSignumShift2);
-constexpr uint32_t cgSignumSelect1d = (cgSignumZero  << cgSignumShift1) | (cgSignumMinus << cgSignumShift0) | (cgSignumMinus << cgSignumShift2);
-constexpr uint32_t cgSignumSelect1e = (cgSignumPlus  << cgSignumShift1) | (cgSignumZero  << cgSignumShift0) | (cgSignumZero  << cgSignumShift2);
-constexpr uint32_t cgSignumSelect1f = (cgSignumMinus << cgSignumShift1) | (cgSignumZero  << cgSignumShift0) | (cgSignumZero  << cgSignumShift2);
-constexpr uint32_t cgSignumSelect1g = (cgSignumZero  << cgSignumShift1) | (cgSignumPlus  << cgSignumShift0) | (cgSignumMinus << cgSignumShift2);
-constexpr uint32_t cgSignumSelect1h = (cgSignumZero  << cgSignumShift1) | (cgSignumMinus << cgSignumShift0) | (cgSignumPlus  << cgSignumShift2);
-
-// Otherwise select 2, no need for checking and thus no constants.
-
-constexpr uint32_t calculateSignum(float const distances[3], float const aEpsilonDistanceFromSide) noexcept {  // TODO optim kibontja a ciklust, de ciklus miatt nincs reg
-  uint32_t result = 0u;
-  for(int32_t i = 0; i < 3; ++i) {
-    int32_t tmp = (distances[i] > aEpsilonDistanceFromSide ? cgSignumPlus : cgSignumZero);
-    tmp = (distances[i] < - aEpsilonDistanceFromSide ? cgSignumMinus : tmp);
-    result |= tmp << (cgSignumShift1 * i);
-  }
-  return result;
-}
-
-// TODO consider Eigen3 for CUDA register usage. It indexes even for dot product.
-// Consider substituting it with simple custom classes if local memory does not fit L1.
-
-void calculateIntersectionParameter(
-  CudaConstTriangle const aShape
-, Eigen::Vector3f const &aIntersectionVector
-, float const aDistanceCornerNfromOtherPlane[3]
-, uint32_t const aSignumShapeFromOtherPlane
-, float &aIntersectionParameterA
-, float &aIntersectionParameterB) noexcept {
-  int32_t indexCommon, indexA, indexB;
-  if(aSignumShapeFromOtherPlane == cgSignumSelect0a
-  || aSignumShapeFromOtherPlane == cgSignumSelect0b
-  || aSignumShapeFromOtherPlane == cgSignumSelect0c
-  || aSignumShapeFromOtherPlane == cgSignumSelect0d
-  || aSignumShapeFromOtherPlane == cgSignumSelect0e
-  || aSignumShapeFromOtherPlane == cgSignumSelect0f
-  || aSignumShapeFromOtherPlane == cgSignumSelect0g
-  || aSignumShapeFromOtherPlane == cgSignumSelect0h) {
-    indexCommon = 0; indexA = 1; indexB = 2;
-  }
-  else if(aSignumShapeFromOtherPlane == cgSignumSelect1a
-  || aSignumShapeFromOtherPlane == cgSignumSelect1b
-  || aSignumShapeFromOtherPlane == cgSignumSelect1c
-  || aSignumShapeFromOtherPlane == cgSignumSelect1d
-  || aSignumShapeFromOtherPlane == cgSignumSelect1e
-  || aSignumShapeFromOtherPlane == cgSignumSelect1f
-  || aSignumShapeFromOtherPlane == cgSignumSelect1g
-  || aSignumShapeFromOtherPlane == cgSignumSelect1h) {
-    indexCommon = 1; indexA = 0; indexB = 2;
-  }
-  else {
-    indexCommon = 2; indexA = 1; indexB = 0;
-  }
-  float vertexProjections[3];
-  for(int32_t i = 0; i < 3; ++i) {
-    vertexProjections[i] = aIntersectionVector.dot(aShape[i]);
-  }
-  aIntersectionParameterA =
-   vertexProjections[indexA]
- + (vertexProjections[indexCommon] - vertexProjections[indexA])
- * aDistanceCornerNfromOtherPlane[indexA]
- / (aDistanceCornerNfromOtherPlane[indexA] - aDistanceCornerNfromOtherPlane[indexCommon]);
-  aIntersectionParameterB = 
-   vertexProjections[indexB]
- + (vertexProjections[indexCommon] - vertexProjections[indexB])
- * aDistanceCornerNfromOtherPlane[indexB]
- / (aDistanceCornerNfromOtherPlane[indexB] - aDistanceCornerNfromOtherPlane[indexCommon]);
-}
-
-bool doesIntersectInAngle(CudaConstTriangle const aShape1, CudaConstTriangle const aShape2, bool const aEpsilonDistanceFromSide) noexcept { // Entry point
-  // TODO consider perhaps min(harmonic mean of sides or area/perimeter of the triangles) for basis of epsilon
-  bool result = false;
-  Eigen::Vector3f shape1normal = (aShape1[1] - aShape1[0]).cross(aShape1[2] - aShape1[0]);
-  Eigen::Vector3f shape2normal = (aShape2[1] - aShape2[0]).cross(aShape2[2] - aShape2[0]);
-  shape1normal.normalize();
-  shape2normal.normalize();
-  float distanceCornerNofShape1FromPlane2[3];
-  float distanceCornerNofShape2FromPlane1[3];
-  for(int32_t i = 0; i < 3; ++i) {
-    distanceCornerNofShape1FromPlane2[i] = shape2normal.dot(aShape1[i] - aShape2[0]);
-    distanceCornerNofShape2FromPlane1[i] = shape1normal.dot(aShape2[i] - aShape1[0]);
-  }
-  uint32_t signumShape1FromPlane2 = calculateSignum(distanceCornerNofShape1FromPlane2, aEpsilonDistanceFromSide); // These contain info about relation of each point and the other plane.
-  uint32_t signumShape2FromPlane1 = calculateSignum(distanceCornerNofShape2FromPlane1, aEpsilonDistanceFromSide);
-  if(signumShape1FromPlane2 == cgSignumAllPlus || signumShape1FromPlane2 == cgSignumAllMinus || signumShape2FromPlane1 == cgSignumAllPlus || signumShape2FromPlane1 == cgSignumAllMinus) {
-    // Nothing to do: one triangle is completely on the one side of the other's plane
-  }
-  else {
-    Eigen::Vector3f intersectionVector = shape1normal.cross(shape2normal);
-    if(fabs(intersectionVector.norm()) > cgEpsilonPlaneIntersectionSine && signumShape1FromPlane2 != cgSignumAllZero && signumShape2FromPlane1 != cgSignumAllZero) { // Real intersection, planes are not identical, and both triangles touch the common line.
-      intersectionVector.normalize();
-      float intersectionParameterAshape1;
-      float intersectionParameterBshape1;
-      float intersectionParameterAshape2;
-      float intersectionParameterBshape2;
-      calculateIntersectionParameter(aShape1, intersectionVector, distanceCornerNofShape1FromPlane2, signumShape1FromPlane2, intersectionParameterAshape1, intersectionParameterBshape1); // The two parameters will contain the locations of the touching point.
-      calculateIntersectionParameter(aShape2, intersectionVector, distanceCornerNofShape2FromPlane1, signumShape2FromPlane1, intersectionParameterAshape2, intersectionParameterBshape2);
-      if(intersectionParameterAshape1 > intersectionParameterBshape1) {
-        std::swap(intersectionParameterAshape1, intersectionParameterBshape1);
-      }
-      else { // nothing to do
-      }
-      if(intersectionParameterAshape2 > intersectionParameterBshape2) {
-        std::swap(intersectionParameterAshape2, intersectionParameterBshape2);
-      }
-      else { // nothing to do
-      }
-      if(intersectionParameterAshape1 - aEpsilonDistanceFromSide <= intersectionParameterAshape2 && intersectionParameterAshape2 <= intersectionParameterBshape1 + aEpsilonDistanceFromSide // Epsilons make possible to check for triangles with corner-corner or corner-edge touch.
-      || intersectionParameterAshape1 - aEpsilonDistanceFromSide <= intersectionParameterBshape2 && intersectionParameterBshape2 <= intersectionParameterBshape1 + aEpsilonDistanceFromSide
-      || intersectionParameterAshape2 - aEpsilonDistanceFromSide <= intersectionParameterAshape1 && intersectionParameterAshape1 <= intersectionParameterBshape2 + aEpsilonDistanceFromSide
-      || intersectionParameterAshape2 - aEpsilonDistanceFromSide <= intersectionParameterBshape1 && intersectionParameterBshape1 <= intersectionParameterBshape2 + aEpsilonDistanceFromSide) {
-        result = true;
-      }
-      else { // Nothing to do
-      }
-    }
-    else { // Nothing to do, because we ignore coplanar triangles.
-    }     //  Meshes are assumed to be continuous, so other triangles will yield the necessary checks.
-  }
-  return result;
-}
 
 Eigen::Matrix3f randomTransform() {
   std::default_random_engine generator;
@@ -187,10 +45,30 @@ Eigen::Matrix3f randomTransform() {
   return result;
 }
 
+void divideLargeTriangles(std::list<Triangle> &aMesh) {
+  for(auto &triangle : aMesh) {
+    float maxSide = (triangle[0] - triangle[1]).norm();
+    maxSide = std::max(maxSide, (triangle[0] - triangle[2]).norm());
+    maxSide = std::max(maxSide, (triangle[1] - triangle[2]).norm());
+    while(maxSide > cgMaxTriangleSide) {
+      auto divider0 = (triangle[1] + triangle[2]) / 2.0f;
+      auto divider1 = (triangle[0] + triangle[2]) / 2.0f;
+      auto divider2 = (triangle[1] + triangle[0]) / 2.0f;
+      auto original1 = triangle[1];
+      auto original2 = triangle[2];
+      triangle[1] = divider2;
+      triangle[2] = divider1;
+      aMesh.push_back({divider1, divider0, original2});
+      aMesh.push_back({divider0, divider2, original1});
+      aMesh.push_back({divider0, divider1, divider2});
+      maxSide /= 2.0f;
+    }
+  }
+}
+
 auto readMesh(std::string const &aFilename, Eigen::Vector3f aTranslation, Eigen::Matrix3f const &aTransform) {
-  Mesh result;
+  std::list<Triangle> work;
   stl_reader::StlMesh<float, int32_t> mesh(aFilename);
-  result.reserve(mesh.num_tris());
   for(int32_t indexTriangle = 0; indexTriangle < mesh.num_tris(); ++indexTriangle) {
     Triangle triangle;
     for(int32_t indexCorner = 0; indexCorner < 3; ++indexCorner) {
@@ -201,9 +79,12 @@ auto readMesh(std::string const &aFilename, Eigen::Vector3f aTranslation, Eigen:
       }
       triangle[indexCorner] = /*aTransform **/ (in + aTranslation);
     }
-    result.push_back(triangle);
+    work.push_back(triangle);
   }
-  return result;
+  std::cout << "bef: " << work.size();
+  divideLargeTriangles(work);
+  std::cout << " aft: " << work.size() << '\n';
+  return Mesh(work.cbegin(), work.cend());
 }
 
 auto readMesh(char const * const aFilename, Eigen::Matrix3f const &aTransform) {
@@ -233,16 +114,6 @@ void writeMesh(Mesh const &aMesh1, Mesh const &aMesh2, char const * const aFilen
     out << "endloop\nendfacet\n";
   }
   out << "endsolid Exported from Blender-2.82 (sub 7)\n";
-}
-
-void intersect(Mesh const &aMesh1, Mesh const &aMesh2, float const aMedianSideSizeHarmonic) {
-  size_t intersectionCount = 0u;
-  for(auto const &item1 : aMesh1) {
-    for(auto const &item2 : aMesh2) {
-      intersectionCount += (doesIntersectInAngle(item1.data(), item2.data(), aMedianSideSizeHarmonic * cgEpsilonDistanceFromSideFactor) ? 1u : 0u);
-    }
-  }
-  std::cout << "intersects: " << intersectionCount << '\n';
 }
 
 float calculateDistanceSum(std::unordered_set<uint32_t> const &aIndices, std::deque<Vertex> const &aVertices) {
@@ -497,13 +368,15 @@ void check(Mesh const &aMesh1, Mesh const &aMesh2) {
   auto approximate2 = approximate(aMesh2, medianSideSizeHarmonic);
   auto sphereSizes1 = calculateSpheres(approximate1);
   auto sphereSizes2 = calculateSpheres(approximate2);
+  for(auto i : sphereSizes1) {
+    std::cout << i << ' ';
+  }
+  for(auto i : sphereSizes2) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
   auto distance = calculateDistance(approximate1, approximate2);
   std::cout << "dist: " << distance << '\n';
-  if(distance < maxSide) {
-    intersect(aMesh1, aMesh2, medianSideSizeHarmonic);
-  }
-  else { // nothing to do
-  }
   writeMesh(toTetras(approximate1, medianSideSizeHarmonic), toTetras(approximate2, medianSideSizeHarmonic), "points.stl");
   writeMesh(toSpheres(approximate1, sphereSizes1), toSpheres(approximate2, sphereSizes2), "spheres.stl");
 }
@@ -511,18 +384,14 @@ void check(Mesh const &aMesh1, Mesh const &aMesh2) {
 int main(int argc, char **argv) {
   int ret = 0;
   if(argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " <filenameIn> [filenameOut]\n";
+    std::cerr << "Usage: " << argv[0] << " <filenameIn>\n";
     ret = 1;
   }
   else {
     try {
       auto transform = randomTransform();
       auto [mesh1, mesh2] = readMesh(argv[1], transform);
-      if(argc >= 3) {
-        writeMesh(mesh1, mesh2, argv[2]);
-      }
-      else { // nothing to do
-      }
+      writeMesh(mesh1, mesh2, "out.stl");
       check(mesh1, mesh2);
     }
     catch(std::exception &e) {
